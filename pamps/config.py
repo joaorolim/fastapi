c90b9d5feabf8c5f83dfccdaf9b3fff8dd79079a
joaorolim <pjrolim@yahoo.com.br>
2024-02-17 11:30:35 -0300

    Create DB Connection

---
+++ pamps/config.py
@@ -0,0 +1,17 @@
+"""Settings module"""
+import os
+
+from dynaconf import Dynaconf
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+print(HERE)
+
+settings = Dynaconf(
+    envvar_prefix="pamps",
+    preload=[os.path.join(HERE, "default.toml")],
+    settings_files=["settings.toml", ".secrets.toml"],
+    environments=["development", "production", "testing"],
+    env_switcher="pamps_env",
+    load_dotenv=False,
+)
